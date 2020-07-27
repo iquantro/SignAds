3369da859ed5a9a698355923a415e656b301790a
ak1ash <aravishankar885@gmail.com>
2020-07-27 10:31:29 +0530

    Update settings.py

--- SignAds/settings.py
+++ SignAds/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'djoser',
-    'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework',
 ]
