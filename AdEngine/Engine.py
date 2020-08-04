7afb5d343620a5dab1c97565404367dc8425bc5e
Akash Ravishankar <aravishankar885@gmail.com>
2020-08-04 18:19:44 +0530

    Logo generation on template working

--- AdEngine/Engine.py
+++ AdEngine/Engine.py
@@ -1,12 +1,16 @@
 from django.template import loader
 from AdRecords.models import Image
-from distutils.dir_util import copy_tree
 from accounts.models import AdvertiserProfile
+from distutils.dir_util import copy_tree
 
 
 class Engine:
     def logo_engine(self, request, image_property_id):
         global advertiser, logo_description
+        global dest, img_name
+        dest = 'E:/SignAds/AdRecords/templates/AdTemplates/demo/assets/'
+        media_path = 'E:/SignAds/media/'
+        asset_str = 'assets/'
         image_path_list = []
         image_description_list = []
         ad_info = AdvertiserProfile.objects.all()
@@ -14,23 +18,25 @@ class Engine:
         for i in image_id_info:
             image_path_list.append(i.image.path)
             image_description_list.append(i.image_description)
+            img_name = str(i.image)
         for desc_val in image_description_list:
             logo_description = desc_val
         for ad_name in ad_info:
             advertiser = ad_name.advertiser_name
-        for path in image_path_list:
-            image_location = path
-            template = loader.get_template('AdTemplates/demo/demo.html')
-            context = {
+
+        copy_tree(media_path, dest)
+        image_location = asset_str+img_name
+        template = loader.get_template('AdTemplates/demo/demo.html')
+        context = {
                 'image_location': image_location,
                 'Advertiser': advertiser,
                 'logo_description': logo_description,
-            }
-            phase_one_ad = template.render(context, request)
-            src = 'AdRecords/templates/AdTemplates/demo'
-            dest = 'AdAssets/{0}/Phase1'.format(advertiser)
-            copy_tree(src, dest)
-            with open('{0}/demo.html'.format(dest), 'w') as f:
-                f.write(phase_one_ad)
+        }
+        phase_one_ad = template.render(context, request)
+        src = 'AdRecords/templates/AdTemplates/demo'
+        ad_dest = 'AdAssets/{0}/Phase1'.format(advertiser)
+        copy_tree(src, ad_dest)
+        with open('{0}/demo.html'.format(ad_dest), 'w') as f:
+            f.write(phase_one_ad)
 
         return True
