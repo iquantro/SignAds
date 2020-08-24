d441366451d1e64c9c12f7cde70a41b0c7c73d2e
nithinkash <nitinkashyapa@gmail.com>
2020-08-24 11:24:29 +0530

    Phase Two completed

--- ClientApi/urls.py
+++ ClientApi/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import AdService
+from .views import AdService, PhaseTwo
 
 urlpatterns = [
     url(r'^Adservice', AdService.as_view(), name='ClientApi'),
+    url(r'^PhaseTwo', PhaseTwo.as_view(), name='PhaseTwo'),
 ]
