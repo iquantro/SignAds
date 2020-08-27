350db131e7908d9ebbff83c8f9b763e43f7eadb7
Akash Ravishankar <aravishankar885@gmail.com>
2020-08-27 17:55:10 +0530

    Flipping engine for Phase one

--- AdEngine/Engine.py
+++ AdEngine/Engine.py
@@ -1,58 +1,21 @@
-from django.template import loader
-from AdRecords.models import Image, Text
+from AdRecords.models import Text
 from accounts.models import AdvertiserProfile, PhaseDB, userProfile
-from distutils.dir_util import copy_tree
-import shutil
-import json
 from Video_Engine.VideoEngine import VideoEngine
 from NeuralNetEngine.generate import TextGen
+from FlippingEngine.phase_one import FlippingEngine
 
 class Engine:
     def phase_one_engine(self, request, image_property_id):
-        global image_engine_path_json
-        image_engine_path_json = "/home/nithin/Startup/SignAds/AdEngine/paths.json"
-        global advertiser, logo_description, advertiser_desc
-        global dest, img_name
-        with open(image_engine_path_json, "r") as rf:
-            paths = json.load(rf)
-        dest = paths['paths']['asset_destination_path']
-        media_path = paths['paths']['media_dir_path']
-        asset_str = paths['paths']['relative_assets_dir_path']
-        image_path_list = []
-        image_description_list = []
-        ad_info = AdvertiserProfile.objects.all()
-        image_id_info = Image.objects.filter(image_property_id=image_property_id)
-        for image_val in image_id_info:
-            image_path_list.append(image_val.image.path)
-            image_description_list.append(image_val.image_description)
-            img_name = str(image_val.image)
-        for desc_val in image_description_list:
-            logo_description = desc_val
-        for ad_name in ad_info:
-            advertiser = ad_name.advertiser_name
-            advertiser_desc = ad_name.advertiser_description
-
-        shutil.copy(media_path + img_name, dest)
-        image_location = asset_str + img_name
-        template = loader.get_template(paths['paths']['relative_template_html_file_path'])
-        context = {
-            'image_location': image_location,
-            'Advertiser': advertiser,
-            'logo_description': logo_description,
-        }
-        phase_one_ad = template.render(context, request)
-        src = paths['paths']['relative_template_dir_path']
-        ad_dest = 'AdAssets/{0}/Phase1'.format(advertiser)
-        copy_tree(src, ad_dest)
-        with open('{0}/demo.html'.format(ad_dest), 'w') as f:
-            f.write(phase_one_ad)
+        phase_one_engine_object = FlippingEngine()
+        phase_one_engine_object.flip_generator(request, image_property_id)
         #phase one tracker
         user_info = userProfile.objects.all()
         advertiser_info = AdvertiserProfile.objects.all()
         phase1_advertiser = [str(phase1_ad.advertiser_name) for phase1_ad in advertiser_info][0]
         phase1_user = [str(phase1_user.user_email) for phase1_user in user_info][0]
         phase1_val = "Phase-1"
-        phase_data = PhaseDB(phase_position=phase1_val, phase_user_email=phase1_user, phase_advertiser_name=phase1_advertiser)
+        phase_data = PhaseDB(phase_position=phase1_val, phase_user_email=phase1_user,
+                             phase_advertiser_name=phase1_advertiser)
         phase_data.save()
         return True
 
@@ -90,4 +53,3 @@ class Engine:
                              phase_advertiser_name=phase3_advertiser)
         phase_data.save()
         return True
-
---
+++ FlippingEngine/phase_one.py
@@ -0,0 +1,46 @@
+from distutils.dir_util import copy_tree
+import shutil
+from accounts.models import AdvertiserProfile
+from AdRecords.models import Image
+import json
+from django.template import loader
+
+class FlippingEngine:
+    def flip_generator(self, request, image_property_id):
+        global image_engine_path_json
+        image_engine_path_json = "E:/SignAds/AdEngine/paths.json"
+        global advertiser, logo_description, advertiser_desc
+        global dest, img_name
+        with open(image_engine_path_json, "r") as rf:
+            paths = json.load(rf)
+        dest = paths['paths']['asset_destination_path']
+        media_path = paths['paths']['media_dir_path']
+        asset_str = paths['paths']['relative_assets_dir_path']
+        image_path_list = []
+        image_description_list = []
+        ad_info = AdvertiserProfile.objects.all()
+        image_id_info = Image.objects.filter(image_property_id=image_property_id)
+        for image_val in image_id_info:
+            image_path_list.append(image_val.image.path)
+            image_description_list.append(image_val.image_description)
+            img_name = str(image_val.image)
+        for desc_val in image_description_list:
+            logo_description = desc_val
+        for ad_name in ad_info:
+            advertiser = ad_name.advertiser_name
+            advertiser_desc = ad_name.advertiser_description
+
+        shutil.copy(media_path + img_name, dest)
+        image_location = asset_str + img_name
+        template = loader.get_template(paths['paths']['relative_template_html_file_path'])
+        context = {
+            'image_location': image_location,
+            'Advertiser': advertiser,
+            'logo_description': logo_description,
+        }
+        phase_one_ad = template.render(context, request)
+        src = paths['paths']['relative_template_dir_path']
+        ad_dest = 'AdAssets/{0}/Phase1'.format(advertiser)
+        copy_tree(src, ad_dest)
+        with open('{0}/demo.html'.format(ad_dest), 'w') as f:
+            f.write(phase_one_ad)
